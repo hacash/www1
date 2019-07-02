a63a8a02d55a3b3769be004c83cf850712d5cef2
yangjiePro <yangjie@jojoin.com>
2019-07-02 12:33:41 +0800

    up

--- jscss/main.js
+++ jscss/main.js
@@ -24,3 +24,30 @@ window.onclick = function(){
 
 })();
 
+
+
+function checkImgs(clsn) {
+  const imgs = Array.from(document.querySelectorAll("."+clsn));
+  imgs.forEach(item => io.observe(item));
+}
+
+function loadImg(el) {
+  if (!el.src) {
+    const source = el.dataset.src;
+    el.src = source;
+  }
+}
+
+const io = new IntersectionObserver(ioes => {
+  ioes.forEach(ioe => {
+    const el = ioe.target;
+    const intersectionRatio = ioe.intersectionRatio;
+    if (intersectionRatio > 0 && intersectionRatio <= 1) {
+      loadImg(el);
+    }
+    el.onload = el.onerror = () => io.unobserve(el);
+  });
+});
+
+
+checkImgs("lazy")
